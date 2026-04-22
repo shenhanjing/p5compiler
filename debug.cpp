dca9dae254c5dd872ddbf094f6fd1f9d7b23a032
shenhanjing <54545001+shenhanjing@users.noreply.github.com>
2026-04-22 18:27:24 +0800

    Update debug.cpp

--- debug.cpp
+++ debug.cpp
@@ -797,3 +797,53 @@ In file included from /usr1/p4c/GenSimv2/include/uint.hpp:14,
       |              ^~~~~~~~~~
 /usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:19668:42: note:   template argument deduction/substitution failed:
 19668 |     DECLARE_UINT_SHALLOW_CONVERTING_CTOR(TelemetryInfo_S)
+      |                                          ^~~~~~~~~~~~~~~
+/usr1/p4c/GenSimv2/include/project_macros.hpp:164:14: note: in definition of macro ‘DECLARE_UINT_SHALLOW_CONVERTING_CTOR’
+  164 |     explicit CLASS_NAME(const U& rhs)                                                               \
+      |              ^~~~~~~~~~
+In file included from /usr1/iMA0/output/SD5993/generated_output/SD5993_merged/test_wrapper.cpp:4:
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_pipeline.hpp:289:283: note:   candidate expects 1 argument, 2 provided
+  289 |                             mswitch::CaseClause(mswitch::masked(Part2or4_S{L3_TYPE_IPv4, IpInfo_S{0, L4_PROTOCOL_NONE}}, Part2or4_S{L3_TYPE_MASK, IpInfo_S{0, L4_PROTOCOL_NONE}}), mswitch::masked(0, 0), mswitch::masked(Part6_S{TelemetryInfo_S{TELEMETRY_TYPE_IFIT_DT, IfitInfo_S{0, 0}}}, Part6_S{TelemetryInfo_S{(TELEMETRY_TYPE_MASK - 1), IfitInfo_S{0, 4}}})),  // tag=2
+      |                                                                                                                                                                                                                                                                                           ^
+In file included from /usr1/iMA0/output/SD5993/generated_output/SD5993_merged/test_wrapper.cpp:1:
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:19667:5: note: candidate: ‘p5Sim::TelemetryInfo_S::TelemetryInfo_S(p5Sim::TelemetryInfo_S&&)’
+19667 |     TelemetryInfo_S(TelemetryInfo_S&&) noexcept = default;
+      |     ^~~~~~~~~~~~~~~
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:19667:5: note:   candidate expects 1 argument, 2 provided
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:19666:5: note: candidate: ‘p5Sim::TelemetryInfo_S::TelemetryInfo_S(const p5Sim::TelemetryInfo_S&)’
+19666 |     TelemetryInfo_S(const TelemetryInfo_S&) = default;
+      |     ^~~~~~~~~~~~~~~
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:19666:5: note:   candidate expects 1 argument, 2 provided
+In file included from /usr1/iMA0/output/SD5993/generated_output/SD5993_merged/test_wrapper.cpp:4:
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_pipeline.hpp:289:284: error: no matching function for call to ‘p5Sim::Part6_S::Part6_S(<brace-enclosed initializer list>)’
+  289 |                             mswitch::CaseClause(mswitch::masked(Part2or4_S{L3_TYPE_IPv4, IpInfo_S{0, L4_PROTOCOL_NONE}}, Part2or4_S{L3_TYPE_MASK, IpInfo_S{0, L4_PROTOCOL_NONE}}), mswitch::masked(0, 0), mswitch::masked(Part6_S{TelemetryInfo_S{TELEMETRY_TYPE_IFIT_DT, IfitInfo_S{0, 0}}}, Part6_S{TelemetryInfo_S{(TELEMETRY_TYPE_MASK - 1), IfitInfo_S{0, 4}}})),  // tag=2
+      |                                                                                                                                                                                                                                                                                            ^
+In file included from /usr1/iMA0/output/SD5993/generated_output/SD5993_merged/test_wrapper.cpp:1:
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:20194:5: note: candidate: ‘p5Sim::Part6_S::Part6_S(uint64_t, uint64_t, std::shared_ptr<std::vector<unsigned char> >, std::shared_ptr<p5Sim::Uint>, bool)’
+20194 |     Part6_S(
+      |     ^~~~~~~
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:20194:5: note:   candidate expects 5 arguments, 1 provided
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:20189:5: note: candidate: ‘p5Sim::Part6_S::Part6_S(uint64_t, p5Sim::ConstEnumTag)’
+20189 |     Part6_S(uint64_t value, ConstEnumTag is_const_enum)
+      |     ^~~~~~~
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:20189:5: note:   candidate expects 2 arguments, 1 provided
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:20184:14: note: candidate: ‘p5Sim::Part6_S::Part6_S(uint64_t)’
+20184 |     explicit Part6_S(uint64_t value)
+      |              ^~~~~~~
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:20184:14: note:   conversion of argument 1 would be ill-formed:
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:20180:14: note: candidate: ‘p5Sim::Part6_S::Part6_S(p5Sim::NullTag)’
+20180 |     explicit Part6_S(NullTag)
+      |              ^~~~~~~
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:20180:14: note:   conversion of argument 1 would be ill-formed:
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:20176:5: note: candidate: ‘p5Sim::Part6_S::Part6_S()’
+20176 |     Part6_S()
+      |     ^~~~~~~
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:20176:5: note:   candidate expects 0 arguments, 1 provided
+In file included from /usr1/p4c/GenSimv2/include/uint.hpp:14,
+                 from /usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:4,
+                 from /usr1/iMA0/output/SD5993/generated_output/SD5993_merged/test_wrapper.cpp:1:
+/usr1/iMA0/output/SD5993/generated_output/SD5993_merged/generated_struct.hpp:20164:42: note: candidate: ‘template<class U, typename std::enable_if<(std::is_base_of<p5Sim::Uint, typename std::decay<_Tp>::type>::value && (! std::is_same<typename std::decay<_Tp>::type, p5Sim::Part6_S>::value)), int>::type <anonymous> > p5Sim::Part6_S::Part6_S(const U&)’
+20164 |     DECLARE_UINT_SHALLOW_CONVERTING_CTOR(Part6_S)
+      |                                          ^~~~~~~
+/usr1/p4c/GenSimv2/include/project_macros.hpp:164:14: note: in definition of macro ‘DECLARE_UINT_SHALLOW_CONVERTING_CTOR’
+  164 |     explicit CLASS_NAME(const U& rhs)                                                               \
